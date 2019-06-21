a2ba5597161e0aa8914c62adc7ede1d65f01a164
Epi-Espinha <jdggouveia@gmail.com>
2019-06-21 15:52:03 +0800

    cleaned the script we have so far

--- MarketData.py
+++ MarketData.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[69]:
-
+#### Start Header -- This is the header and contains the packages needed for this script ####
 
 import datetime as dt
 import matplotlib.pyplot as plt
@@ -13,25 +12,23 @@ import numpy as np
 
 style.use("ggplot")
 
+#### End Header ####
 
-# In[70]:
+#### Start Importing the financial data from the web ####
 
-
+# we define the date range we want
 start = dt.datetime(1962, 1, 3)
 end = dt.datetime(2012, 11, 9)
 
+# we collect the financial date using the web.DataReader command
 df = web.DataReader("^GSPC", "yahoo", start, end)
-print(df.tail())
+print(df.tail()) # print the last entries to see if your data is correct
 
+# Find the type of data which was stored in df
+type(df)
 
-# In[71]:
-
-
-type(df) # This was that the df is a dataframe type
-
-
-# In[72]:
-
-
+# save the dataframe as csv
 pd.DataFrame(df).to_csv("GSPC.csv")
+
+#### End Importing ####
 
